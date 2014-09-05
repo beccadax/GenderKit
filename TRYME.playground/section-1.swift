b44cf81c23e3df89536308677c06453efba00067
Brent Royal-Gordon <brent@architechies.com>
2014-09-05 16:01:08 -0700

    Add TRYME.playground

---
+++ TRYME.playground/section-1.swift
@@ -0,0 +1,59 @@
+import UIKit
+import GenderKit
+import XCPlayground
+
+// 
+// GenderKit includes a Gender enum which represents a
+// person's gender. Gender can be Male, Female, or 
+// Other, with Other carrying a custom set of pronouns.
+// 
+let gender = Gender.Male
+//let gender = Gender.Female
+//let gender = Gender.Other(description: "Agender", pronouns: PronounSet(subject: "They", object: "Them", possessiveDeterminer: "Their", determiner: "Theirs"))
+gender.description
+
+// 
+// Easily construct sentences with pronouns appropriate
+// for a particular gender using the pronoun helper 
+// functions.
+//
+"\(They(gender)) let me go to \(their(gender)) house."
+
+//
+// You can make your own model classes conform to 
+// PronounReferable to allow for easier use of the 
+// pronoun helper functions.
+//
+class User: PronounReferable {
+    var userGender = gender
+    
+    var pronouns: PronounSet {
+        return userGender.pronouns
+    }
+}
+let myUser = User()
+
+"\(They(myUser)) took me to \(their(myUser)) house."
+
+// 
+// The GenderPickerView gives you a simple control for 
+// choosing a gender.
+// 
+let picker = GenderPickerView(frame: CGRect(x: 0, y: 0, width: 300, height: 24))
+//picker.explicitlyAllowsNone = true
+picker.selectedGender = gender
+XCPShowView("GenderPickerView", picker)
+// 
+// Tapping "Other" displays a gender designer allowing 
+// the user to specify their preferred gender identity 
+// and pronouns, but this feature can't be demoed in 
+// a Playground.
+// 
+
+// 
+// Genders are RawRepresentable as Strings; the 
+// representation is "M" for Male, "F" for Female, or 
+// "O(description)|(pronoun JSON data)" for Other.
+// 
+let raw = gender.toRaw()
+Gender.fromRaw(raw)!
