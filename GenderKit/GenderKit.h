0d399f25a3dbab05812465aa359dce201e366b18
Brent Royal-Gordon <brent@architechies.com>
2014-09-04 23:23:55 -0700

    Initial Commit

---
+++ GenderKit/GenderKit.h
@@ -0,0 +1,19 @@
+//
+//  GenderKit.h
+//  GenderKit
+//
+//  Created by Brent Royal-Gordon on 9/4/14.
+//  Copyright (c) 2014 Architechies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GenderKit.
+FOUNDATION_EXPORT double GenderKitVersionNumber;
+
+//! Project version string for GenderKit.
+FOUNDATION_EXPORT const unsigned char GenderKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenderKit/PublicHeader.h>
+
+
