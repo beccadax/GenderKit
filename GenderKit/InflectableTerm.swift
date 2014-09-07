5b12f329db7ce4b66f0f1064b74f791a412f983a
Brent Royal-Gordon <brent@architechies.com>
2014-09-06 19:11:39 -0700

    Add InflectableTerm constants for English pronouns

--- GenderKit/InflectableTerm.swift
+++ GenderKit/InflectableTerm.swift
@@ -9,24 +9,36 @@
 /// An InflectableTerm represents a term that should be dynamically inflected 
 /// based on appropriate genders. It is intended to look up the term in the 
 /// gender lexicon.
-struct InflectableTerm: Inflectable {
-    let inflector: (GenderLexicon) -> String
-    let index: Int
+public struct InflectableTerm: Inflectable {
+    var inflector: (GenderLexicon) -> String
+    var index: Int
     
-    init(inflector: (GenderLexicon) -> String) {
+    public init(inflector: (GenderLexicon) -> String) {
         self.init(index: 0, inflector: inflector)
     }
     
-    init(index: Int, inflector: (GenderLexicon) -> String) {
+    public init(index: Int, inflector: (GenderLexicon) -> String) {
         self.inflector = inflector
         self.index = index
     }
     
-    subscript (i: Int) -> InflectableTerm {
+    public subscript (i: Int) -> InflectableTerm {
         return InflectableTerm(index: i, inflector: inflector)
     }
     
-    func inflectedString(genderLexicons lexicons: [GenderLexicon]) -> String {
+    public func inflectedString(genderLexicons lexicons: [GenderLexicon]) -> String {
         return inflector(lexicons[index])
     }
 }
+
+public let They = InflectableTerm { $0.subjectPronoun }
+public let they = InflectableTerm { $0.subjectPronoun.lowercaseString }
+
+public let Them = InflectableTerm { $0.objectPronoun }
+public let them = InflectableTerm { $0.objectPronoun.lowercaseString }
+
+public let Their = InflectableTerm { $0.possessiveDeterminerPronoun }
+public let their = InflectableTerm { $0.possessiveDeterminerPronoun.lowercaseString }
+
+public let Theirs = InflectableTerm { $0.possessivePronoun }
+public let theirs = InflectableTerm { $0.possessivePronoun.lowercaseString }
